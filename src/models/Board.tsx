70223562ccb35ebb0900ce7c966057c1879c41ee
Philip Rasmusson <studiobigmouth@gmail.com>
2021-08-18 15:57:58 +0200

    main game complete

--- src/models/Board.tsx
+++ src/models/Board.tsx
@@ -8,7 +8,11 @@ export const Board = () => {
 
   const handleClick = (i: any) => {
     const sq = squares.slice()
-    sq[i] = 'X'
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+    playerOne ? sq[i] = 'X' : sq[i] = 'O'
+    setPlayerOne(!playerOne)
     setSquares(sq)
   }
 
@@ -19,12 +23,41 @@ export const Board = () => {
     />)
   }
 
-  const status = 'Next player: X'
 
+  const status = () => {
+    const winner = calculateWinner(squares)
+    let status
+    if (winner) {
+      status = 'Winner: ' + winner
+    } else {
+      status = 'Next player: ' + (playerOne ? 'X' : 'O')
+    }
+
+    return (status)
+  }
+
+  function calculateWinner(squares: any) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i]
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a]
+      }
+    }
+  }
 
   return (
     <div>
-      <div className="status">{status}</div>
+      <div className="status">{status()}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
--- src/models/Square.tsx
+++ src/models/Square.tsx
@@ -3,14 +3,10 @@ import { useState } from 'react'
 export const Square = (props: { value: any, onClick: any }) => {
 
 
-  const [squareState, setSquareState] = useState(props.value)
-
 
   return (
-    <div>
-      <button className="square" onClick={() => { { props.onClick() } }}>
-        {props.value}
-      </button>
-    </div>
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
   )
 }
